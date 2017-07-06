49dd310bb304c6361d3161dbf42dcd886742bfc9
wdalmorra <wddsouza@inf.ufpel.edu.br>
2017-07-06 14:35:28 -0300

    Minimax em aparente correto funcionamento.

--- Siege.py
+++ Siege.py
@@ -3,7 +3,7 @@ import json
 import sys
 import random
 
-MAX_DEPTH = 4
+MAX_DEPTH = 2
 
 with open("vizinhos.json") as v_file:
 	neighbors = json.load(v_file)
@@ -31,13 +31,13 @@ class Board():
 		if me == "y":
 			if "h1" in self.yellow:
 				h += 50
-			h += n_y - n_r
+			h += (n_y - n_r) * 10
 			return h
 
 		else:
 			if "h1" in self.yellow:
 				h -= 50
-			h += n_r - n_y
+			h += (n_r - n_y) * 10
 			return h
 
 		# return random.randint(0,100)
@@ -45,7 +45,7 @@ class Board():
 		
 	def startGame(self):
 		self.yellow = ["g1","g2","g3","g4","g5","g6","g7","g8","f1","f2","f3","f4","f5","f6","f7","f8"]
-		self.red = ["a1","a2","a3","a4","a5","a6","a7","a8","a9","a10","a11","a12","a13","a14","a15","a16"]
+		self.red = ["a1","a2","a3","a4","a5","a6","a7","a8","a9","a10","a11","a12","a13","e6","a15","a16"]
 
 	def setYellow(self, yellow):
 		self.yellow = deepcopy(yellow)
@@ -63,44 +63,46 @@ class Board():
 		if color == "y":
 			for p in self.yellow:
 				# MOVIMENTO COMUM
-				for n in neighbors[p]:
-					if n not in self.yellow and n not in self.red:
-						newYellow = [x if x != p else str(n) for x in self.yellow]
-						newBoard = Board()
-						newBoard.setYellow(newYellow)
-						newBoard.setRed(self.red)
-						children.append(newBoard)
-				# CONSUMO DE PECA OPONENTE
-				for n in eatings[p]:
-					if n[1] in self.red and (n[2] not in self.yellow and n[2] not in self.red):
-						newYellow = [x if x != p else str(n[2]) for x in self.yellow]
-						newRed = [x if x != n[1] else None for x in self.red]
-						newBoard = Board()
-						newBoard.setYellow(newYellow)
-						newBoard.setRed(newRed)
-						children.append(newBoard)
+				if p != None:
+					for n in neighbors[p]:
+						if n not in self.yellow and n not in self.red:
+							newYellow = [x if x != p else str(n) for x in self.yellow]
+							newBoard = Board()
+							newBoard.setYellow(newYellow)
+							newBoard.setRed(self.red)
+							children.append(newBoard)
+					# CONSUMO DE PECA OPONENTE
+					for n in eatings[p]:
+						if n[1] in self.red and (n[2] not in self.yellow and n[2] not in self.red):
+							newYellow = [x if x != p else str(n[2]) for x in self.yellow]
+							newRed = [x if x != n[1] else None for x in self.red]
+							newBoard = Board()
+							newBoard.setYellow(newYellow)
+							newBoard.setRed(newRed)
+							children.append(newBoard)
 
 		elif color == "r":
 
 			for p in self.red:
 				# MOVIMENTO COMUM
-				for n in neighbors[p]:
-					if n not in self.yellow and n not in self.red:
-						newRed = [x if x != p else str(n) for x in self.red]
-						newBoard = Board()
-						newBoard.setYellow(self.yellow)
-						newBoard.setRed(newRed)
-						children.append(newBoard)
+				if p != None:
+					for n in neighbors[p]:
+						if n not in self.yellow and n not in self.red:
+							newRed = [x if x != p else str(n) for x in self.red]
+							newBoard = Board()
+							newBoard.setYellow(self.yellow)
+							newBoard.setRed(newRed)
+							children.append(newBoard)
 
-				# CONSUMO DE PECA OPONENTE
-				for n in eatings[p]:
-					if n[1] in self.yellow and (n[2] not in self.yellow and n[2] not in self.red):
-						newYellow = [x if x != n[1] else None for x in self.yellow]
-						newRed = [x if x != p else str(n[2]) for x in self.red]
-						newBoard = Board()
-						newBoard.setYellow(newYellow)
-						newBoard.setRed(newRed)
-						children.append(newBoard)
+					# CONSUMO DE PECA OPONENTE
+					for n in eatings[p]:
+						if n[1] in self.yellow and (n[2] not in self.yellow and n[2] not in self.red):
+							newYellow = [x if x != n[1] else None for x in self.yellow]
+							newRed = [x if x != p else str(n[2]) for x in self.red]
+							newBoard = Board()
+							newBoard.setYellow(newYellow)
+							newBoard.setRed(newRed)
+							children.append(newBoard)
 
 		return children
 
@@ -134,12 +136,13 @@ def minimax(board, turn, me, alpha, beta, depth, tab):
 				print tab + "BOARD: " + str(n_board)
 				if aval < beta:
 					beta = aval
+					beta_board = c
 				if alpha >= beta:
 					print tab + "Returned: " + str(beta)
-					return beta, c
+					return beta, beta_board
 					
 			print tab + "Returned: " + str(aval)
-			return aval, c
+			return aval, beta_board
 
 		else:
 			print tab + "Max"
@@ -164,12 +167,13 @@ def minimax(board, turn, me, alpha, beta, depth, tab):
 				print tab + "BOARD: " + str(n_board)
 				if aval > alpha:
 					alpha = aval
+					alpha_board = c
 				if alpha >= beta:
 					print tab + "Returned: " + str(alpha)
-					return alpha, c
+					return alpha, alpha_board
 
 			print tab + "Returned: " + str(aval)
-			return aval, c 
+			return aval, alpha_board 
 	else:
 		print tab + "Returned: " + str(estado)
 		return estado, board
