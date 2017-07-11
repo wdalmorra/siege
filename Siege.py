35ff8b20649698cb30018223c8e3a870f1927b00
wdalmorra <wddsouza@inf.ufpel.edu.br>
2017-07-11 13:48:16 -0300

    codigo para execucao do jogo implementado. possivle problema no massacre

--- Siege.py
+++ Siege.py
@@ -11,7 +11,29 @@ with open("vizinhos.json") as v_file:
 with open("coletas.json") as c_file:
 	eatings = json.load(c_file)
 
+def check_move(m_from, m_to, color, board):
+
+	global neighbors
+	global eatings
+
+	if m_from in neighbors:
+		if m_to in neighbors[m_from] and m_to not in board.yellow and m_to not in board.red:
+			return True
+		for pos in eatings[m_from]:
+			if pos[2] == m_to:
+				if color == "y":
+					if pos[1] in board.red and (pos[2] not in board.yellow and pos[2] not in board.red):
+						return True
+				elif color == "r":
+					if pos[1] in board.yellow and (pos[2] not in board.yellow and pos[2] not in board.red):
+						return True
+	return False
+
 def massacre(board, color):
+
+	global neighbors
+	global eatings
+
 	sequence = []
 	ate = False
 	if color == "y":
@@ -36,8 +58,8 @@ def massacre(board, color):
 			if p != None:
 				for n in eatings[p]:
 					if n[1] in board.yellow and (n[2] not in board.yellow and n[2] not in board.red):
-						newYellow = [x if x != n[1] else None for x in self.yellow]
-						newRed = [x if x != p else str(n[2]) for x in self.red]
+						newYellow = [x if x != n[1] else None for x in board.yellow]
+						newRed = [x if x != p else str(n[2]) for x in board.red]
 						newBoard = Board()
 						newBoard.setYellow(newYellow)
 						newBoard.setRed(newRed)
@@ -86,7 +108,7 @@ class Board():
 		
 	def startGame(self):
 		self.yellow = ["g1","g2","g3","g4","g5","g6","g7","g8","f1","f2","f3","f4","f5","f6","f7","f8"]
-		self.red = ["a1","a2","a3","a4","a5","a6","a7","a8","a9","a10","a11","a12","a13","e6","c11","e8"]
+		self.red = ["b1","a2","a3","a4","a5","a6","a7","a8","a9","a10","a11","a12","a13","a14","a15","a16"]
 
 	def setYellow(self, yellow):
 		self.yellow = deepcopy(yellow)
@@ -170,10 +192,10 @@ class Board():
 
 def minimax(board, turn, me, alpha, beta, depth, tab):
 	# print "nova iteracao da recursao"
-	print tab + "Depth: " + str(depth)
-	print tab + str(board)
+	# print tab + "Depth: " + str(depth)
+	# print tab + str(board)
 	estado = board.avaliacao(me)
-	print tab + "Avaliacao: " + str(estado)
+	# print tab + "Avaliacao: " + str(estado)
 
 	# ends = [n for n in range(-1,2)]
 	# if estado in ends:
@@ -184,30 +206,30 @@ def minimax(board, turn, me, alpha, beta, depth, tab):
 		# print "Turn: " + turn
 		# print "Me: " + me
 		if turn != me:
-			print tab + "Min"
+			# print tab + "Min"
 
 			aval = sys.maxint
 			beta_board = None
 			children = board.move(turn)
 			# print "CHILDREN: " + str(len(children))
 			for c in children:
-				print tab + str(c[-1])
+				# print tab + str(c[-1])
 				avalminmax, n_board = minimax(c[-1], me, me, alpha, beta, depth+1, tab+"\t")
 				aval = min(aval, avalminmax)
 				# print depth
-				print tab + "BOARD: " + str(n_board)
+				# print tab + "BOARD: " + str(n_board)
 				if aval < beta:
 					beta = aval
 					beta_board = c
 				if alpha >= beta:
-					print tab + "Returned: " + str(beta)
+					# print tab + "Returned: " + str(beta)
 					return beta, beta_board
 					
-			print tab + "Returned: " + str(aval)
+			# print tab + "Returned: " + str(aval)
 			return aval, beta_board
 
 		else:
-			print tab + "Max"
+			# print tab + "Max"
 
 			aval = -sys.maxint-1
 			# print board.board
@@ -222,41 +244,119 @@ def minimax(board, turn, me, alpha, beta, depth, tab):
 
 			# print "CHILDREN: " + str(len(children))
 			for c in children:
-				print tab + str(c[-1])
+				# print tab + str(c[-1])
 				avalminmax, n_board = minimax(c[-1], turn, me, alpha, beta, depth+1, tab+"\t")
 				aval = max(aval, avalminmax)
 				# print depth
-				print tab + "BOARD: " + str(n_board)
+				# print tab + "BOARD: " + str(n_board)
 				if aval > alpha:
 					alpha = aval
 					alpha_board = c
 				if alpha >= beta:
-					print tab + "Returned: " + str(alpha)
+					# print tab + "Returned: " + str(alpha)
 					return alpha, alpha_board
 
-			print tab + "Returned: " + str(aval)
+			# print tab + "Returned: " + str(aval)
 			return aval, alpha_board 
 	else:
-		print tab + "Returned: " + str(estado)
+		# print tab + "Returned: " + str(estado)
 		return estado, board
 def main():
-	b = Board()
-	b.startGame()
+	start = Board()
+	start.startGame()
 	# print b
-
+	opponent = raw_input("Which color are you? (r/y):  ")
 	# children = b.move("r")
-	me = "y"
-
-	aval, sequence = minimax(b, "y", me, -sys.maxint-1,sys.maxint, 0, "")
-	print aval
 	
-	if len(sequence) > 1:
-		print "MASSACRE"
+	me = "r" if opponent == "y" else "y"
 
-	for b in sequence:
-		print b
+	finished = False
+
+	if me == "r":
+		print "I go first!"
+		aval, sequence = minimax(start, "r", me, -sys.maxint-1,sys.maxint, 0, "")
+		sequence_size = len(sequence)
+		if sequence_size > 1:
+			print "MASSACRE!!!!!!!!"
+		print "My move(s) is(are): "
+		for a, b in zip(start.red, sequence[0].red):
+			if a != b:
+				print "From: " + a
+				print "To: "  + b
+				break
+		if sequence_size > 1:
+			for i in range(sequence_size-1):
+				for a, b in zip(sequence[i].red, sequence[i+1].red):
+					if a != b:
+						print "From: " + a
+						print "To: "  + b
+						break
+		current_board = sequence[-1]
+	else:
+		print "You go first!"
 
 
+	while not finished:
+		print "Your Turn"
+		print current_board
+		data = raw_input("Enter your move (<from><space><to>): ")
+		data = data.split(" ")
+		m_from = data[0]
+		m_to = data[1]
+
+		valid =	check_move(m_from, m_to, opponent, current_board)
+
+		while not valid:
+			print "Invalid move! Try Again"
+
+			data = raw_input("Enter your move (<from><space><to>): ")
+			data = data.split(" ")
+			m_from = data[0]
+			m_to = data[1]
+
+			valid =	check_move(m_from, m_to, opponent, current_board)
+
+
+		if opponent == "y":
+			current_board.yellow = [x if x != m_from else m_to for x in current_board.yellow]
+		else:
+			current_board.red = [x if x != m_from else m_to for x in current_board.red]
+
+		aval = current_board.avaliacao(me)
+
+		if aval < -100000:
+			print "You win"
+			break
+
+		print "My Turn"
+		print current_board
+		aval, sequence = minimax(current_board, me, me, -sys.maxint-1,sys.maxint, 0, "")
+		sequence_size = len(sequence)
+		if sequence_size > 1:
+			print "MASSACRE!!!!!!!!"
+		print "My move(s) is(are): "
+		for a, b in zip(current_board.red, sequence[0].red):
+			if a != b:
+				print "From: " + a
+				print "To: "  + b
+				break
+		if sequence_size > 1:
+			for i in range(sequence_size-1):
+				for a, b in zip(sequence[i].red, sequence[i+1].red):
+					print sequence[i]
+					print sequence[i+1]
+					if a != b:
+						print "From: " + a
+						print "To: "  + b
+						break
+		
+		current_board = sequence[-1]
+		
+		aval = current_board.avaliacao(me)
+		
+		if aval > 100000:
+			print "I win"
+			break
 
 if __name__ == '__main__':
 	main()
